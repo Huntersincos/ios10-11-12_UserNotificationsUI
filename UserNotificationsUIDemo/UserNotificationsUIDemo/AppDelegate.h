dc5a8de0dea28ab0591f97bcfc3ced0ee13a07a2
wenze <wenze@wenzedeAir.workgroup>
2018-10-16 10:21:33 +0800

    ios10-11-12_UserNotificationsUI

---
+++ UserNotificationsUIDemo/UserNotificationsUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserNotificationsUIDemo
+//
+//  Created by wenze on 2018/10/16.
+//  Copyright © 2018年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserNotificationsUIDemo/UserNotificationsUIDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserNotificationsUIDemo
+//
+//  Created by wenze on 2018/10/16.
+//  Copyright © 2018年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
